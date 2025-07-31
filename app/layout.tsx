39cf3bc008a4069908b8ca51900f2eea7ff5ee11
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-07-31 22:44:17 +0530

    Misc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
