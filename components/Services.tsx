d22a6b84e995b069f60450f8c083bc56faed8e19
Surabhil Ssk <surabhil.s.kumar@gmail.com>
2025-08-03 14:12:29 +0530

    Servces page init

---
+++ components/Services.tsx
@@ -0,0 +1,9 @@
+export default function Services() {
+  return (
+    <div className="h-screen">
+      <div className="text-center pt-14 text-4xl font-semibold">
+        Our Services
+      </div>
+    </div>
+  );
+}
